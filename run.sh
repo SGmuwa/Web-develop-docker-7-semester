c82a08cc03628dc8e6d356be00392fa2768cf390
SG_Muwa <motherlode.muwa@gmail.com>
2019-11-30 23:29:54 +0300

    Более мягкий sudo.

--- run.sh
+++ run.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-  then echo Please run as root
-  exit
-fi
 git submodule update --recursive
 if [[ $? -ne 0 ]]; then
   echo Failed to update submodules. Try fix...
   git reset --hard HEAD
-  echo [submodule "frontend/Web-develop-frontend-7-semester"] > .gitmodules
-  echo 	path = frontend/Web-develop-frontend-7-semester >> .gitmodules
-  echo 	url = https://github.com/SGmuwa/Web-develop-frontend-7-semester.git >> .gitmodules
-  echo [submodule "backend/Web-develop-backend-7-semester"] >> .gitmodules
-  echo 	path = backend/Web-develop-backend-7-semester >> .gitmodules
-  echo 	url = https://github.com/SGmuwa/Web-develop-backend-7-semester.git >> .gitmodules
+  echo '[submodule "frontend/Web-develop-frontend-7-semester"]' > .gitmodules
+  echo '	path = frontend/Web-develop-frontend-7-semester' >> .gitmodules
+  echo '	url = https://github.com/SGmuwa/Web-develop-frontend-7-semester.git' >> .gitmodules
+  echo '[submodule "backend/Web-develop-backend-7-semester"]' >> .gitmodules
+  echo '	path = backend/Web-develop-backend-7-semester' >> .gitmodules
+  echo '	url = https://github.com/SGmuwa/Web-develop-backend-7-semester.git' >> .gitmodules
   git submodule update --recursive
   git reset --hard HEAD
 fi
@@ -26,6 +22,6 @@ else
   #/usr/bin/mvn package
 fi
 cd ../../
-#docker-compose build
-#docker-compose up
+#sudo docker-compose build
+#sudo docker-compose up
 echo good bye.
