168758c673be2b94727b9563527d3e3932bbf693
SG_Muwa <motherlode.muwa@gmail.com>
2019-12-01 00:05:28 +0300

    Настроен run.sh для любого debian.

--- run.sh
+++ run.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-git submodule update --recursive
-if [[ $? -ne 0 ]]; then
-  echo Failed to update submodules. Try fix...
-  git reset --hard HEAD
-  echo '[submodule "frontend/Web-develop-frontend-7-semester"]' > .gitmodules
-  echo '	path = frontend/Web-develop-frontend-7-semester' >> .gitmodules
-  echo '	url = https://github.com/SGmuwa/Web-develop-frontend-7-semester.git' >> .gitmodules
-  echo '[submodule "backend/Web-develop-backend-7-semester"]' >> .gitmodules
-  echo '	path = backend/Web-develop-backend-7-semester' >> .gitmodules
-  echo '	url = https://github.com/SGmuwa/Web-develop-backend-7-semester.git' >> .gitmodules
-  git submodule update --recursive
-  git reset --hard HEAD
-fi
-cd ./backend/Web-develop-backend-7-semester/
+git submodule update --recursive --init
 /usr/bin/mvn --version
 if [[ $? -ne 0 ]]; then
   echo Maven problem.
   echo Install apt: sudo apt install maven
   echo or visit https://maven.apache.org/download.cgi
-else
-  #/usr/bin/mvn package
+  exit
 fi
+cd ./backend/Web-develop-backend-7-semester/
+/usr/bin/mvn package
 cd ../../
-#sudo docker-compose build
-#sudo docker-compose up
+docker-compose --version
+if [[ $? -ne 0 ]]; then
+  echo docker-compose problem.
+  echo Install apt: sudo apt install docker-compose
+  echo or visit https://docs.docker.com/compose/install/
+  exit
+fi
+sudo docker-compose build
+sudo docker-compose up
 echo good bye.
